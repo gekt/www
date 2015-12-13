bda9495dd10ec5184e23fecd825a6353d9807597
ArthixFR <julien.thomas018@live.fr>
2015-12-13 03:17:02 +0100

    Cleanup phase 1

--- index.php
+++ index.php
@@ -1,40 +1,37 @@
-<?php 
-include 'check.php';  //include page php
-include 'nb_online.php';
-include 'resultatonline.php';
+<!DOCTYPE html>
+
+<?php
+    include 'check.php';
+    include 'nb_online.php';
+    include 'resultatonline.php';
 ?>
 
 <?php
-// on teste si le visiteur a soumis le formulaire de connexion
-if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') { // on teste si le visiteur a soumis le formulaire de connexion
     if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
+        $base = @mysql_connect ('localhost', 'root', '');
+        mysql_select_db ('espace_membres', $base);
 
-    $base = @mysql_connect ('localhost', 'root', '');
-    mysql_select_db ('espace_membres', $base);
+        // on teste si une entrée de la base contient ce couple login / pass
+        $sql = 'SELECT count(*) FROM membre WHERE login="'.mysql_real_escape_string($_POST['login']).'" AND pass_md5="'.mysql_real_escape_string(md5($_POST['pass'])).'"';
+        $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+        $data = mysql_fetch_array($req);
 
-    // on teste si une entrée de la base contient ce couple login / pass
-    $sql = 'SELECT count(*) FROM membre WHERE login="'.mysql_real_escape_string($_POST['login']).'" AND pass_md5="'.mysql_real_escape_string(md5($_POST['pass'])).'"';
-    $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-    $data = mysql_fetch_array($req);
+        mysql_free_result($req);
+        mysql_close();
 
-    mysql_free_result($req);
-    mysql_close();
-
-    // si on obtient une réponse, alors l'utilisateur est un membre
-    if ($data[0] == 1) {
-        session_start();
-        $_SESSION['login'] = $_POST['login'];
-        header('Location: membre.php');
-        exit();
-    }
-    // si on ne trouve aucune réponse, le visiteur s'est trompé soit dans son login, soit dans son mot de passe
-    elseif ($data[0] == 0) {
-        $erreur = "<script language='Javascript'> alert('Compte non reconnu.'); </script>";
-    }
-    // sinon, alors la, il y a un gros problème :)
-    else {
-        $erreur = 'Probème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
-    }
+        if ($data[0] == 1) { // si on obtient une réponse, alors l'utilisateur est un membre
+            session_start();
+            $_SESSION['login'] = $_POST['login'];
+            header('Location: membre.php');
+            exit();
+        }
+        elseif ($data[0] == 0) { // si on ne trouve aucune réponse, le visiteur s'est trompé soit dans son login, soit dans son mot de passe
+            $erreur = "<script language='Javascript'> alert('Pseudo ou mot de passe incorrect.'); </script>";
+        }
+        else { // sinon, alors la, il y a un gros problème :)
+            $erreur = 'Probème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
+        }
     }
     else {
     $erreur = 'Au moins un des champs est vide.';
@@ -42,8 +39,6 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
 }
 ?>
 
-
-<!DOCTYPE html>
 <html>
     <head>
         <title>Mon site de test</title>
@@ -51,73 +46,62 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-
-    <?php
+<?php
     session_start();
-    if (isset($_SESSION['login']))
-    {
-        // le login a été enregistré dans la session, j'affiche le lien du profil
-       // echo "tu es co";
-    ?> 
-         <aside class="entete">
+    if (isset($_SESSION['login'])) {
+    // le login a été enregistré dans la session, j'affiche le lien du profil
+?> 
+        <aside class="entete">
                 <a href="deconnexion.php">
                     <img src="img/bouton_deco.png">
                 </a>
             <div  class="player">Joueurs en ligne GTA: <?php echo $check ?></div>
             <div  class="player">Joueurs en ligne PIXELMON: <?php echo $check2 ?></div>
             <p>il y a <?php echo $data[0]; ?> personne actuellement sur le site !</p>
-         </aside>
- 
-        <?php
+        </aside>
+<?php
     }
-    else
-    {
-        ?>
-       <aside class="entete">
-              <form action="index.php" method="post">
-              Pseudo: <input type="text" name="login" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>"><br />
-              MDP: <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
-              <input type="submit" name="connexion" value="Connexion"><br>
-              <input type="button" name="s'inscrire" value="S'inscrire" onclick="self.location.href='inscription.php'">
-               <!--<a href="inscription.php">Vous inscrire</a> -->
+    else {
+?>
+        <!-- pas de login en session : proposer la connexion !-->
+        <aside class="entete">
+            <form action="index.php" method="post">
+                Pseudo: <input type="text" name="login" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>"><br />
+                MDP: <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
+                <input type="submit" name="connexion" value="Connexion"><br>
+                <input type="button" name="s'inscrire" value="S'inscrire" onclick="self.location.href='inscription.php'">
             </form>
-            <?php
+<?php
                 if (isset($erreur)) echo '<br /><br />',$erreur;
-            ?>
-            <div  class="player">Joueurs en ligne GTA: <?php echo $check ?></div>
-            <div  class="player">Joueurs en ligne PIXELMON: <?php echo $check2 ?></div>
+?>
+            <div class="player">Joueurs en ligne GTA: <?php echo $check ?></div>
+            <div class="player">Joueurs en ligne PIXELMON: <?php echo $check2 ?></div>
             <p>il y a <?php echo $data[0]; ?> personne actuellement sur le site !</p>
        </aside>
-
-        <?php
-        // pas de login en session : proposer la connexion
-        //echo "tu n'est pas co";
-    }
-        ?>
-        <nav>
-            <ul>
-                <li style="color: blue;"><a href="index.php">Accueil</a></li>
-                <li style="color: red;"><a href="#">Forum</a></li>
-                <li style="color: green;"><a href="#">Boutique</a></li>
-                <?php
-                if (isset($_SESSION['login'])) {
-                    ?>    
-                    <li style="color: purple;"><a href="membre.php">Espace Membre</a></li>
-                    <li style="color: purple;"><a href="shoutbox/shoutbox.php">Shoutbox</a></li>
-                     <?php
-                }
-                ?>
-            </ul>
-        </nav>
-
-        <?php
-        /* PHP OK */
-        ?>
+<?php
+    }     
+?>
+    <nav>
+        <ul>
+            <li style="color: blue;"><a href="index.php">Accueil</a></li>
+            <li style="color: red;"><a href="#">Forum</a></li>
+            <li style="color: green;"><a href="#">Boutique</a></li>
+<?php
+            if (isset($_SESSION['login'])) {
+?>    
+                <li style="color: purple;"><a href="membre.php">Espace Membre</a></li>
+                <li style="color: purple;"><a href="shoutbox/shoutbox.php">Shoutbox</a></li>
+<?php
+            }
+?>
+        </ul>
+    </nav>
         <aside class="contenu">
-        <p><?php
-        // on inclut l'affichage de nos news
-        include ('news.php');
-        ?></p>
+            <p>
+<?php
+            include ('news.php'); // on inclut l'affichage de nos news
+?>
+            </p>
         </aside>
 
         <aside class="menu">
--- membre.php
+++ membre.php
@@ -1,122 +1,99 @@
+<!DOCTYPE html>
 
 <?php
-session_start();
-if (!isset($_SESSION['login'])) {
-	header ('Location: index.php');
-	exit();
-}
+    session_start();
+    if (!isset($_SESSION['login'])) {
+        header ('Location: index.php');
+        exit();
+    }
+
+    include 'check.php';
+    include 'nb_online.php';
+    include 'resultatonline.php';
+    include 'sessionid.php';
+    include 'badgecheck.php';
+    include 'bouton_newsAdmin.php';
+    include("geoloc/geoipcity.inc");
+    include("geoloc/geoipregionvars.php");
+
+    $_SESSION['id'] = $id;
+    $pseudo =  $_SESSION['login'];
 ?>
-<?php 
-include 'check.php';  //include page php
-include 'nb_online.php';
-include 'resultatonline.php';
-include 'sessionid.php';
-include 'badgecheck.php';
-include 'bouton_newsAdmin.php';
-?>
-<?php
-$_SESSION['id'] = $id;
-$pseudo =  $_SESSION['login'];
-?>
+
 <html>
-<head>
-<title>Espace membre</title>
+    <head>
+        <title>Espace membre</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+    </head>
 
-<body>
+    <body>
 <?php
-include("geoloc/geoipcity.inc");
-include("geoloc/geoipregionvars.php");
+        $gi = geoip_open(realpath("geoloc/GeoLiteCity.dat"),GEOIP_STANDARD);
 
-$gi = geoip_open(realpath("geoloc/GeoLiteCity.dat"),GEOIP_STANDARD);
-
-if ($_SERVER['REMOTE_ADDR'] != '::1') {
-	$record = geoip_record_by_addr($gi,$_SERVER['REMOTE_ADDR']);
-	$code = geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']);
-	$pays = $record->country_name;
-	if (!in_array($code, array('FR', 'CH', 'BE', 'CA'), true)) {
-		$code = "unknown";
-	}
-} else {
-	$pays = "Switzerland";
-	$code = "ch";
-}
-
-//echo "hey tu viens de "; 
-//echo $record->country_name . "\n";
-
-geoip_close($gi);
-
-?>
-
-
-  <aside class="entete">
-  	<!--<img src="https://minotar.net/avatar/<?php echo $pseudo ?>/20"/>-->
-  	<p>hey tu viens de: <?php echo $pays . "\n"; echo '<img height="20" weight="30" src="/img/' . $code . '.png">';?> </p>
-	<p>Bienvenue <?php echo htmlentities(trim($_SESSION['login'])); ?> !</p>
-	<?php
- 	@mysql_connect("localhost", "root", ""); // Connexion à la base de données
- 	@mysql_select_db("espace_membres"); // Sélection de la base de données
-	 $reponse = @mysql_query ("SELECT * FROM membre WHERE login='" . $_SESSION['login'] . "' "); // Requête SQL
-    
- 	while ($donnees = @mysql_fetch_array($reponse)) // On boucle pour afficher toutes les données et on met toutes données dans un tableau
- 	{
-	?>
-	<p>Vos points <?php echo $donnees['nb_points'];?> !<a href="points_add.php"><img height="20" weight="30" src="img/plus.png"/></a><a href="points_del.php"><img height="23" weight="30" src="img/moins.png"/></a></p>
-	<?php
- 	}
- 	//@mysql_close(); // On oubli pas de déconnecter la base de données
-	?>
-    <?php //messagerie
-
-        // on prépare une requete SQL cherchant tous les titres, les dates ainsi que l'auteur des messages pour le membre connecté
-        $sql = 'SELECT titre, date, membre.login as expediteur, messages.id as id_message FROM messages, membre WHERE id_destinataire="'.$_SESSION['id'].'" AND id_expediteur=membre.id ORDER BY date DESC';
-        // lancement de la requete SQL
-        $req = @mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-        $nb = @mysql_num_rows($req);
-
-        if ($nb == 0) {
-            ?>
-    <p>Vous n'avez aucun message.</p>
-    <?php
-        }
-        elseif ($nb == 1) {
-        ?>
-            <p>Vous avez <?php echo $nb; ?> message</p>
-        <?php
+        if ($_SERVER['REMOTE_ADDR'] != '::1') {
+            $record = geoip_record_by_addr($gi,$_SERVER['REMOTE_ADDR']);
+            $code = geoip_country_code_by_addr($gi, $_SERVER['REMOTE_ADDR']);
+            $pays = $record->country_name;
+            if (!in_array($code, array('FR', 'CH', 'BE', 'CA'), true)) {
+                $code = "Unknown";
+            }
         }
         else {
-        // si on a des messages, on affiche la date, un lien vers la page lire.php ainsi que le titre et l'auteur du message
-        ?>
-           <p>Vous avez <?php echo $nb; ?> messages</p>
-        <?php
-        while ($data = @mysql_fetch_array($req)) {
-            ?>
-          <!-- <p>-->
-          <!--  <?php echo $data['date'] ?>: <a href="lire.php?id_message=<?php echo $data['id_message'];?>"> <?php echo stripslashes(htmlentities(trim($data['titre'])))?> </a>[ Message de <?php echo stripslashes(htmlentities(trim($data['expediteur'])))?> ]<br />-->
-          <!--  </p>-->
-        <!--echo $data['date'] , ' - <a href="lire.php?id_message=' , $data['id_message'] , '">' , stripslashes(htmlentities(trim($data['titre']))) , '</a> [ Message de ' , stripslashes(htmlentities(trim($data['expediteur']))) , ' ]<br />';-->
-        <?php
+            $pays = "Localhost";
+            $code = "Unknown";
         }
-    }
-    @mysql_free_result($req);
-    @mysql_close();
+        geoip_close($gi);
+?>
 
-    //fin de messagerie?>
-	<p>Badge 1 : <?php echo $badge1;?></p>
-	<a href="deconnexion.php">
-	<img src="img/bouton_deco.png" title="Se deconnecter">
-	</a>
-    <a href="envoyer.php">
-    <img src="img/message.png" title="Envoyer un message" height="27" weight="30" >
-    </a>
-    <?php echo $boutonNews; ?>
+        <aside class="entete">
+            <p>hey tu viens de: <?php echo $pays . "\n"; echo '<img height="20" weight="30" src="/img/' . $code . '.png">';?> </p>
+            <p>Bienvenue <?php echo htmlentities(trim($_SESSION['login'])); ?> !</p>
+<?php
+            @mysql_connect("localhost", "root", ""); // Connexion à la base de données
+            @mysql_select_db("espace_membres"); // Sélection de la base de données
+            $reponse = @mysql_query ("SELECT * FROM membre WHERE login='" . $_SESSION['login'] . "' "); // Requête SQL
+            while ($donnees = @mysql_fetch_array($reponse)) { // On boucle pour afficher toutes les données et on met toutes données dans un tableau
+?>
+                <p>Vos points <?php echo $donnees['nb_points'];?> !<a href="points_add.php"><img height="20" weight="30" src="img/plus.png"/></a><a href="points_del.php"><img height="23" weight="30" src="img/moins.png"/></a></p>
+<?php
+            }
 
+            $sql = 'SELECT titre, date, membre.login as expediteur, messages.id as id_message FROM messages, membre WHERE id_destinataire="'.$_SESSION['id'].'" AND id_expediteur=membre.id ORDER BY date DESC'; // on prépare une requete SQL cherchant tous les titres, les dates ainsi que l'auteur des messages pour le membre connecté
+            $req = @mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error()); // lancement de la requete SQL
+            $nb = @mysql_num_rows($req);
+
+            if ($nb == 0) { // Si pas de message
+?>
+                <p>Vous n'avez aucun message.</p>
+                <?php
+            }
+            elseif ($nb == 1) {
+?>
+                <p>Vous avez <?php echo $nb; ?> message</p>
+<?php
+            }
+            else { // si on a des messages, on affiche la date, un lien vers la page lire.php ainsi que le titre et l'auteur du message
+?>
+                <p>Vous avez <?php echo $nb; ?> messages</p>
+<?php
+            }
+
+            @mysql_free_result($req);
+            @mysql_close();
+?>
+
+            <p>Badge 1 : <?php echo $badge1;?></p>
+            <a href="deconnexion.php">
+                <img src="img/bouton_deco.png" title="Se deconnecter">
+            </a>
+            <a href="envoyer.php">
+                <img src="img/message.png" title="Envoyer un message" height="27" weight="30" >
+            </a>
+            <?php echo $boutonNews; ?>
             <div  class="player">Joueurs en ligne GTA: <?php echo $check ?></div>
             <div  class="player">Joueurs en ligne PIXELMON: <?php echo $check2 ?></div>
-       </aside>
+        </aside>
 
         <nav>
             <ul>
@@ -128,19 +105,14 @@ geoip_close($gi);
             </ul>
         </nav>
 
-        <?php
-        /* PHP */
-        ?>
         <aside class="contenu">
             <p><?php include 'listemessage.php';?></p>
-
         </aside>
 
         <aside class="menu">
         </aside>
-
-</body>
-    <footer>
-        <aside>Copyright Kevin Torre helped by Julien Thomas and Alexandre Pedro</aside>
-    </footer>
+    </body>
+        <footer>
+            <aside>Copyright Kevin Torre helped by Julien Thomas and Alexandre Pedro</aside>
+        </footer>
 </html>
